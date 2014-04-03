3bf95ff6aba5ad32556cf582bcbd15282a6537b2
Evan D Young <evan.d.young@gmail.com>
2014-04-03 12:43:21 -0600

    updating get.woeid.r

--- get.woeid.r
+++ get.woeid.r
@@ -1,11 +1,12 @@
 require(XML)
+library(twitteR)
+require(twitterR)
 
 get.woeid = function (query, key) {
 
-
     query = gsub(" ", "\\%20", query)
 
-    url = paste0("http://where.yahooapis.com/v1/places.q(", query, ")?appid=", key, "--")
+    url = paste0("http://where.yahooapis.com/v1/places.q(", query, ")?appid=", key)
 
     doc = xmlTreeParse(url, useInternalNodes = TRUE)
 
@@ -13,5 +14,34 @@ get.woeid = function (query, key) {
 
     num = xmlSApply(top[[1]]["woeid"][[1]], xmlValue)
 
-    return num[[1]]
+    return(num[[1]])
+
+}
+
+setup.OAuth = function () {
+    reqURL = 'https://api.twitter.com/oauth/request_token'
+    accessURL = 'https://api.twitter.com/oauth/access_token'
+    authURL = 'https://api.twitter.com/oauth/authorize'
+
+    #collect the info
+    cat('What is your consumer key? \n')
+    consumerKey = readLines(file("stdin"),1)
+    cat('What is your consumer secret? \n')
+    consumerSecret = readLines(file("stdin"),1)
+    cat('What is your Yahoo app ID? \n')
+    yahoo.id = readLines(file("stdin"),1)
+
+    #commence OAuth
+    twitCred <- OAuthFactory$new(consumerKey=consumerKey,
+                                 consumerSecret=consumerSecret,
+                                 requestURL=reqURL,
+                                 accessURL=accessURL,
+                                 authURL=authURL)
+
+    twitCred$handshake()
+
+    registerTwitterOAuth(twitCred)
+
+    cat('Proceed with your use of the twitteR package. Enjoy \n')
+
 }
